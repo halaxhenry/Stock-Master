5e46aa36415a85f6c3fbc69c01bb85df141739e5
seungchul <halaxhenry@gmail.com>
2022-01-28 14:51:16 +0900

    Initial Commit

---
+++ Stock Master/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Stock Master
+//
+//  Created by Seungchul Ha on 2022/01/28.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
